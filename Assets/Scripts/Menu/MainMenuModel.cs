99ddc10833c39c1f3be4df97fa8cd30fa7b5d2f4
Nikita Kirakosyan <nikita.kirakosyan.work@gmail.com>
2022-03-28 19:32:09 +0300

    Second commit update

---
+++ Assets/Scripts/Menu/MainMenuModel.cs
@@ -0,0 +1,14 @@
+/*
+ * author : Kirakosyan Nikita
+ * e-mail : nikita.kirakosyan.work@gmail.com
+ */
+using TMPro;
+using UnityEngine;
+
+namespace BlackMountain.MainMenu
+{
+    public sealed class MainMenuModel : MonoBehaviour
+    {
+        public TextMeshProUGUI versionText = null;
+    }
+}
---
+++ Assets/Scripts/Menu/MainMenuView.cs
@@ -0,0 +1,16 @@
+/*
+ * author : Kirakosyan Nikita
+ * e-mail : nikita.kirakosyan.work@gmail.com
+ */
+using UnityEngine;
+
+namespace BlackMountain.MainMenu
+{
+    public sealed class MainMenuView : MonoBehaviour
+    {
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
